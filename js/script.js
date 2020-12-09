4be11b8ed1c1da01bcd015bde9d967ea70ad37ed
Pamela Ladu <pamela.ladu@gmail.com>
2020-12-09 15:20:23 +0100

    prima imbastitura con commenti, html-js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 var app = new Vue({
 el: '#root',
 data: {
+  logo: ,//inserire img logo
+  backgroundHeader: , //inserire img header (possibile v-bind)
+  headerLists: [ //array di oggetti con proprietà nome
+    {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+  ],
+  footerLists: [], //array dentro array(?)
+  voucher: ,//img voucher
+  downloadStore: ,//img Stores
+  socialIcons: [] ,// array icone
+  inputEmail: "", //collegare v-model
+  emailList: [] , //array dove inserire email del form
 
 },
+methods: {
+  buttonInput: function() {
+    
+  },
+
+
+
+
+}
 
 });
